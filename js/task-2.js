6c81d77773927634e110a804135a3d252c38b7ba
oleh <olegduzh@gmail.com>
2024-11-22 13:23:43 +0200

    hw-4

---
+++ js/task-2.js
@@ -0,0 +1,46 @@
+const calcAverageCalories = function (days) {
+    let totalCalories = 0;
+    const totalDays = Object.keys(days);
+  
+  if (days <= 0) {
+    return totalCalories;
+  }
+
+  for (const values of days) {
+    totalCalories += values.calories;
+    } return totalCalories / totalDays.length;
+  
+} 
+
+
+
+
+
+
+    console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
